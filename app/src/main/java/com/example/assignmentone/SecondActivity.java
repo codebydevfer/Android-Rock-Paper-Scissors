04781de9bfe17a7dce19d09e887add99a8eae447
Fernando <fernandopeandrade@outlook.com>
2025-10-06 12:26:46 -0400

    Adding share button functionality, adding background image, organizing code.

--- app/src/main/java/com/example/assignmentone/SecondActivity.java
+++ app/src/main/java/com/example/assignmentone/SecondActivity.java
@@ -14,25 +14,20 @@ import androidx.core.view.WindowInsetsCompat;
 
 import org.w3c.dom.Text;
 
+//Emojis used
+//Rock: \uD83E\uDEA8
+//Paper: \uD83D\uDCC3
+//Scissors: \u2702\uFE0F
+
 public class SecondActivity extends AppCompatActivity{
 
-    Button homeBtn;
-    Button rock;
-    Button paper;
-    Button scissors;
-    Button restart;
-    TextView welcome;
-    TextView currentPlayerPlay;
-    TextView currentComputerPlay;
-    TextView roundWinner;
-    TextView userScore;
+    //declare buttons, textview, int, intent...
+    Button homeBtn, rock, paper, scissors, restart, shareBtn;
+    TextView welcome, currentPlayerPlay, currentComputerPlay, roundWinner, userScore, userLost;
     int score;
+    Intent shareObj = new Intent();
 
-    //Emojis
-    //Rock: \uD83E\uDEA8
-    //Paper: \uD83D\uDCC3
-    //Scissors: \u2702\uFE0F
-
+    //Home, share and restart buttons
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,6 +43,33 @@ public class SecondActivity extends AppCompatActivity{
             }
 
         });
+
+        shareBtn = findViewById(R.id.share_button);
+        shareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shareObj.setAction(Intent.ACTION_SEND);
+                shareObj.setType("text/plain");
+
+                Intent shareScore = Intent.createChooser(shareObj, null);
+                startActivity(shareScore);
+            }
+        });
+
+        restart = findViewById(R.id.restart_button);
+        restart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentPlayerPlay.setText("");
+                currentComputerPlay.setText("");
+                userLost.setText("");
+                roundWinner.setText("");
+                score = 0;
+                userScore.setText("Your score: " + score);
+            }
+        });
+
+        //Get IDs
         Intent intObj = getIntent();
         String val1 = intObj.getStringExtra("KEY_ONE");
 
@@ -59,12 +81,15 @@ public class SecondActivity extends AppCompatActivity{
 
         roundWinner = findViewById(R.id.round_winner);
 
+        userLost = findViewById(R.id.you_lost_tv);
+
         userScore = findViewById(R.id.user_score);
 
         rock = findViewById(R.id.rock_button);
         paper = findViewById(R.id.paper_button);
         scissors = findViewById(R.id.scissors_buton);
 
+        //Game buttons (rock, paper, scissors)
         rock.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -73,18 +98,21 @@ public class SecondActivity extends AppCompatActivity{
 
                 if (currentComputerPlay.getText().equals("\uD83E\uDEA8")){
                     roundWinner.setText("Draw");
+                    shareObj.putExtra(Intent.EXTRA_TEXT, "Check out my score: " + score);
                 }
                 else if (currentComputerPlay.getText().equals("\uD83D\uDCC3")){
                     roundWinner.setText("Computer Wins");
+                    shareObj.putExtra(Intent.EXTRA_TEXT, "Check out my score: " + score);
                     score = 0;
                     userScore.setText("Your score: " + score);
+                    userLost.setText("You Lose!");
                 }
                 else if (currentComputerPlay.getText().equals("\u2702\uFE0F")){
                     roundWinner.setText("Player Wins");
                     score++;
+                    shareObj.putExtra(Intent.EXTRA_TEXT, "Check out my score: " + score);
                     userScore.setText("Your score: " + score);
                 }
-//                roundWinner.setText("");
             }
         });
 
@@ -96,16 +124,20 @@ public class SecondActivity extends AppCompatActivity{
 
                 if (currentComputerPlay.getText().equals("\uD83D\uDCC3")){
                     roundWinner.setText("Draw");
+                    shareObj.putExtra(Intent.EXTRA_TEXT, "Check out my score: " + score);
                 }
                 else if (currentComputerPlay.getText().equals("\uD83E\uDEA8")){
                     roundWinner.setText("Player Wins");
                     score++;
+                    shareObj.putExtra(Intent.EXTRA_TEXT, "Check out my score: " + score);
                     userScore.setText("Your score: " + score);
                 }
                 else if (currentComputerPlay.getText().equals("\u2702\uFE0F")){
                     roundWinner.setText("Computer Wins");
+                    shareObj.putExtra(Intent.EXTRA_TEXT, "Check out my score: " + score);
                     score = 0;
                     userScore.setText("Your score: " + score);
+                    userLost.setText("You Lose!");
                 }
             }
         });
@@ -118,31 +150,26 @@ public class SecondActivity extends AppCompatActivity{
 
                 if (currentComputerPlay.getText().equals("SCISSORS")){
                     roundWinner.setText("Draw");
+                    shareObj.putExtra(Intent.EXTRA_TEXT, "Check out my score: " + score);
                 }
                 else if (currentComputerPlay.getText().equals("\uD83D\uDCC3")){
                     roundWinner.setText("Player Wins");
                     score++;
+                    shareObj.putExtra(Intent.EXTRA_TEXT, "Check out my score: " + score);
                     userScore.setText("Your score: " + score);
                 }
                 else if (currentComputerPlay.getText().equals("\uD83E\uDEA8")){
                     roundWinner.setText("Computer Wins");
+                    shareObj.putExtra(Intent.EXTRA_TEXT, "Check out my score: " + score);
                     score = 0;
                     userScore.setText("Your score: " + score);
+                    userLost.setText("You Lose!");
                 }
             }
         });
-
-        restart = findViewById(R.id.restart_button);
-        restart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                currentPlayerPlay.setText("");
-                currentComputerPlay.setText("");
-                score = 0;
-                userScore.setText("Your score: " + score);
-            }
-        });
     }
+
+    //computer play method
     public void computerPlay() {
         double randomValue = Math.random();
 
